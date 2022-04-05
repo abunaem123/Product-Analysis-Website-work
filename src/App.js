d96b7824b2fb71a3d67dfe98fcb96e3ddfb6f470
Abu Naem <123abunaem@gmail.com>
2022-04-06 00:27:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Car Review</h1>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const ExploreBtn = useNavigate()
+    return (
+        <div>
+            <div className='grid grid-cols-2 bg-slate-800 '>
+                <div className='m-auto'>
+                    <h1 className='text-white text-5xl'>CAR CUSTOMER REVIEWS</h1>
+                    <p className='text-gray-200  px-24 mt-4'>Blindsided by a new generation of racers, Lightning McQueen is pushed out of the sport he loves. To get back in the game, he'll need the help of young race technician Cruz Ramirez.</p>
+                    <button className='py-2 px-10 bg-white mt-5 text-lg rounded' onClick={() => ExploreBtn('/about')}>Visite</button>
+                </div>
+                <div>
+                    <img src="/Images/car.png" alt="" />
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Home;
